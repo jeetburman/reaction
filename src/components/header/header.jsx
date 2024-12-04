f13489cff1fb2ff92ddfaeefbaa0a9b26f0c8534
Jeet Burman <jeetburman95@gmail.com>
2024-12-04 12:02:28 +0530

    Added header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.css";
+
+export default function Header(){
+    return (
+        <div className="header-title header-front">
+            <h1>My react App</h1>
+        </div>
+    );
+}
